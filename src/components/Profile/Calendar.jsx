bae30c4c1bb08f3fa2909d660d0042af081737cd
perezpefaur <perezpefaur@uc.cl>
2022-05-24 17:04:03 -0400

    View changes in component

--- src/components/Profile/Calendar.jsx
+++ src/components/Profile/Calendar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react'
 import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
@@ -13,124 +13,125 @@ import {
   AppointmentForm,
   ConfirmationDialog
 } from '@devexpress/dx-react-scheduler-material-ui';
+import CircularProgress from '@mui/material/CircularProgress';
 import Paper from '@mui/material/Paper';
 
 const Calendar = (props) => {
+    const [teacher, setTeacher] = React.useState({});
+    const [loading, setLoading] = React.useState(true);
 
-  // obtengo el dia actual
-  var currentDate = new Date();
-  var dd = String(currentDate.getDate()).padStart(2, '0');
-  var mm = String(currentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = currentDate.getFullYear();
-  currentDate = yyyy + '-' + mm + '-' + dd;
+    useEffect(() => {
+        props.teacher ? setTeacher(props.teacher) : setTeacher({})
+        setLoading(false);
+    }, [props.teacher]);
 
-  // aca va mi conexion con la api para tener los datos
-  const schedulerData = [
-    {
-      title: 'Website Re-Design Plan',
-      startDate: '2022-05-25T09:45',
-      endDate: '2022-05-25T11:45',
-      id: 0,
-      location: 'Room 1',
-    }
-  ];
+    // obtengo el dia actual
+    var currentDate = new Date();
+    var dd = String(currentDate.getDate()).padStart(2, '0');
+    var mm = String(currentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = currentDate.getFullYear();
+    currentDate = yyyy + '-' + mm + '-' + dd;
 
-  // checkeo si soy el current user
-  const isNormalUser = false
+    // aca va mi conexion con la api para tener los datos
+    const schedulerData = [
+        {
+        startDate: '2022-05-25T09:45',
+        endDate: '2022-05-25T11:45',
+        }
+    ];
 
-  // manejo el crear, editar y delete horario
-  const commitChanges = (added, changed, deleted) => {
-    console.log(added);
-  };
+    // checkeo si soy el current user
+    const isNormalUser = false
 
-  // saco y edito componentes inecesarios
-  const BoolEditor = (props) => {
-    return null;
-  };
-  const LabelComponent = (props) => {
-    if (props.text === 'Details') {
-      return <AppointmentForm.Label
-      { ...props} 
-      text="Precio Modulo"
-      />  
-    } else if (props.text === 'More Information') {
-      return null
-    } else if (props.text === '-') {
-      return <AppointmentForm.Label
-      { ...props}
-      />  
-    }
-  };
-  const InputComponent = (props) => {
-    if (props.type === 'titleTextEditor') {
-      return <AppointmentForm.TextEditor
-      { ...props}
-      type='numberEditor'
-      placeholder='Precio'
-      />
-    }
-  };
+    // manejo el crear, editar y delete horario
+    const commitChanges = (added, changed, deleted) => {
+        console.log(added);
+    };
 
-  // cambio el layout
-  const BasicLayout = ({ onFieldChange, appointmentData, ...restProps }) => {
-    return (
-      <AppointmentForm.BasicLayout
-        appointmentData={appointmentData}
-        onFieldChange={onFieldChange}
-        {...restProps}   
-      >
-      </AppointmentForm.BasicLayout>
-    );
-  };
+    // saco y edito componentes inecesarios
+    const BoolEditor = (props) => {
+        return null;
+    };
+    const LabelComponent = (props) => {
+        if (props.text === 'Details') {
+        return null  
+        } else if (props.text === 'More Information') {
+        return null
+        } else if (props.text === '-') {
+        return <AppointmentForm.Label
+        { ...props}
+        />  
+        }
+    };
+    const InputComponent = (props) => {
+        if (props.type === 'titleTextEditor') {
+        return null
+        }
+    };
 
-  // retorno el calendario 
-  return (
-    <>
-    <h1 style={{ 'textAlign': 'center' }}>Horario del Profesor:</h1>
-        <Paper>
-        <Scheduler
-          data={schedulerData}
+    // cambio el layout
+    const BasicLayout = ({ onFieldChange, appointmentData, ...restProps }) => {
+        return (
+        <AppointmentForm.BasicLayout
+            appointmentData={appointmentData}
+            onFieldChange={onFieldChange}
+            {...restProps}   
         >
+        </AppointmentForm.BasicLayout>
+        );
+    };
 
-          <ViewState
-            defaultCurrentDate={currentDate}
-            defaultCurrentViewName="Week"
-          />
 
-          <EditingState
-            onCommitChanges={commitChanges}
-          />
-          <IntegratedEditing />
+    if (loading) return <CircularProgress />
 
-          <DayView
-            startDayHour={7}
-            endDayHour={23}
-          />
-          <WeekView startDayHour={7} endDayHour={23} />
+    // retorno el calendario 
+    return (
+        <>
+        <h1 style={{ 'textAlign': 'center' }}>Horario de {teacher.first_name}:</h1>
+            <Paper>
+            <Scheduler
+            data={schedulerData}
+            >
 
-          <Toolbar />
-          <DateNavigator />
-          <TodayButton />
-          <ViewSwitcher />
-          <ConfirmationDialog />
-          <Appointments />
-          <AppointmentTooltip
-            showCloseButton
-            showOpenButton
-          />
-          <AppointmentForm
-            readOnly={isNormalUser}
-            basicLayoutComponent={BasicLayout}
-            booleanEditorComponent={BoolEditor}
-            labelComponent={LabelComponent}
-            textEditorComponent={InputComponent}
-          />
-        </Scheduler>
-      </Paper>
-      <br />
-      <br />
-      </>
-  )
+            <ViewState
+                defaultCurrentDate={currentDate}
+                defaultCurrentViewName="Week"
+            />
+
+            <EditingState
+                onCommitChanges={commitChanges}
+            />
+            <IntegratedEditing />
+
+            <DayView
+                startDayHour={7}
+                endDayHour={23}
+            />
+            <WeekView startDayHour={7} endDayHour={23} />
+
+            <Toolbar />
+            <DateNavigator />
+            <TodayButton />
+            <ViewSwitcher />
+            <ConfirmationDialog />
+            <Appointments />
+            <AppointmentTooltip
+                showCloseButton
+                showOpenButton
+            />
+            <AppointmentForm
+                readOnly={isNormalUser}
+                basicLayoutComponent={BasicLayout}
+                booleanEditorComponent={BoolEditor}
+                labelComponent={LabelComponent}
+                textEditorComponent={InputComponent}
+            />
+            </Scheduler>
+        </Paper>
+        <br />
+        <br />
+        </>
+    )
 }
 
 export default Calendar
