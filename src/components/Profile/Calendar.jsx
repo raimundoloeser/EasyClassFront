154293307c3f4f398248ad450dc42939188448eb
perezpefaur <perezpefaur@uc.cl>
2022-05-24 17:05:01 -0400

    Eliminate unused imports

--- src/components/Profile/Calendar.jsx
+++ src/components/Profile/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
